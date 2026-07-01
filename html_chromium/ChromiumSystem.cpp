22608e22fea1c96449b9ed78501343747b15a18d
WinterPhoenix <winter@solsticegamestudios.com>
2026-07-01 14:39:13 -0400

    html_chromium: Remove the dark mode hack now that CEF detects it natively
    - CEF 137 tracks the OS dark/light theme at runtime, not just at startup like the hack did (#190)
    - Windows needs the new Win10/11 manifest for this; macOS already gets it from its modern SDK
    - Drops the now-unused registry and Cocoa includes

--- html_chromium/ChromiumSystem.cpp
+++ html_chromium/ChromiumSystem.cpp
@@ -23,59 +23,10 @@ namespace fs = std::filesystem;
 
 #if _WIN32
 	#include <Windows.h>
-	#include <winreg.h>
 #endif
 
 #if __APPLE__
-	#include <Cocoa/Cocoa.h>
 	#include <crt_externs.h>
-#endif
-
-// https://stackoverflow.com/a/70753913
-#ifdef _WIN32
-bool IsDarkMode() {
-	// The value is expected to be a REG_DWORD, which is a signed 32-bit little-endian
-	auto buffer = std::vector<char>(4);
-	auto cbData = static_cast<DWORD>(buffer.size() * sizeof(char));
-	auto res = RegGetValueW(
-		HKEY_CURRENT_USER,
-		L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
-		L"AppsUseLightTheme",
-		RRF_RT_REG_DWORD, // expected value type
-		nullptr,
-		buffer.data(),
-		&cbData);
-
-	if (res != ERROR_SUCCESS) {
-		return false;
-	}
-
-	// convert bytes written to our buffer to an int, assuming little-endian
-	auto i = int(buffer[3] << 24 |
-		buffer[2] << 16 |
-		buffer[1] << 8 |
-		buffer[0]);
-
-	return i == 0;
-}
-#endif
-
-#if __linux__
-bool IsDarkMode() {
-	// TODO: Not exactly sure how you're supposed to do this on Linux. GTK/Qt?
-	return false;
-}
-#endif
-
-// https://github.com/chromium/chromium/blob/main/ui/native_theme/native_theme_mac.mm#L35-L41
-#ifdef __APPLE__
-bool IsDarkMode() {
-	NSAppearanceName appearance =
-		[NSApp.effectiveAppearance bestMatchFromAppearancesWithNames:@[
-			NSAppearanceNameAqua, NSAppearanceNameDarkAqua
-		]];
-	return [appearance isEqual:NSAppearanceNameDarkAqua];
-}
 #endif
 
 // In the CEF browser process (the launched gmod.exe), our own command line carries the player's launch options
@@ -182,13 +133,7 @@ public:
 			command_line->AppendSwitchWithValue( "remote-allow-origins", "http://localhost:" + std::to_string( m_RemoteDebuggingPort ) );
 		}
 
-		// HACK: Force dark mode if the OS says it should be
-		// TODO/BUG: For some reason, OS theme detection is just straight up not working
-		// It might be because we're running in Windowless/OSR mode, but yet, it works fine in CEFClient with `--off-screen-rendering-enabled`, so I'm sort of at a loss
-		// https://github.com/solsticegamestudios/GModPatchTool/issues/190
-		if (IsDarkMode()) {
-			command_line->AppendSwitch( "force-dark-mode" );
-		}
+		// prefers-color-scheme follows the OS natively now (CEF 137+) and tracks runtime theme changes; Windows relies on the SupportedOS manifest
 	}
 
 	void OnRegisterCustomSchemes( CefRawPtr<CefSchemeRegistrar> registrar ) override
